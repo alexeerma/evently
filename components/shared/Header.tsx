91bc9820f4b2c0dee9bf84d87ec2a68b780a7d34
Aleksander <aleksander.eerma@hotmail.com>
2023-12-24 16:08:52 +0200

    Layout and logic for login and signup page added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,7 @@
+import { SignedOut } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "../ui/button"
 
 const Header = () => {
   return (
@@ -8,6 +10,15 @@ const Header = () => {
             <Link href="/" className="w-36">
                 <Image src="/assets/images/logo.svg" width={128} height={38} alt="Evently logo"/>
             </Link>
+            <div className="flex w-32 justify-end gap-3">
+                <SignedOut>
+                    <Button asChild className="rounded-full" size="lg">
+                        <Link href="/sign-in">
+                            Login
+                        </Link>
+                    </Button>
+                </SignedOut>
+            </div>
         </div>
     </header>
   )
