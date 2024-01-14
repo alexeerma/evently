fcadb1799ca7f7cb370664826aa88cad66f8d10b
Aleksander <aleksander.eerma@hotmail.com>
2024-01-14 18:02:12 +0200

    Added Eventform, now needs form handling

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,7 +1,5 @@
-import { OurFileRouter } from "@/app/api/uploadthing/core";
-import { generateComponents } from "@uploadthing/react";
+import { generateReactHelpers } from "@uploadthing/react/hooks";
  
-
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
  
-export const { UploadButton, UploadDropzone, Uploader } =
-  generateComponents<OurFileRouter>();
+export const { useUploadThing, uploadFiles } = generateReactHelpers<OurFileRouter>();
